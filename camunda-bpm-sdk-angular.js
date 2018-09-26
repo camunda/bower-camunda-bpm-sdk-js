0025fbee8a6bd9f762ff5d2f14d8acc059bf4111
camunda-jenkins <ci@camunda.com>
2018-09-26 10:40:25 +0200

    chore(project): release 7.8.10

--- camunda-bpm-sdk-angular.js
+++ camunda-bpm-sdk-angular.js
@@ -2537,7 +2537,7 @@ History.path = 'history';
 
 
 /**
- * Queries for the number of user operation log entries that fulfill the given parameters
+ * Query for user operation log entries that fulfill the given parameters.
  *
  * @param {Object}   [params]
  * @param {String}   [params.processDefinitionId]   Filter by process definition id.
@@ -2561,22 +2561,6 @@ History.path = 'history';
  * @param {Number}   [params.maxResults]            Pagination of results. Specifies the maximum number of results to return. Will return less results if there are no more results left.
  * @param {Function} done
  */
-History.userOperationCount = function(params, done) {
-  if (typeof params === 'function') {
-    done = arguments[0];
-    params = {};
-  }
-
-  return this.http.get(this.path + '/user-operation/count', {
-    data: params,
-    done: done
-  });
-};
-
-/**
- * Queries for user operation log entries that fulfill the given parameters
- * This method takes the same parameters as `History.userOperationCount`.
- */
 History.userOperation = function(params, done) {
   if (typeof params === 'function') {
     done = arguments[0];
@@ -2588,7 +2572,6 @@ History.userOperation = function(params, done) {
     done: done
   });
 };
-
 
 
 /**
@@ -2767,27 +2750,6 @@ History.decisionInstanceCount = function(params, done) {
     done: done
   });
 };
-
-/**
- * Delete historic decision instances asynchronously. With creation of a batch operation.
- *
- * @param params - either list of decision instance ID's or an object corresponding to a decisionInstances
- *                  POST request based query
- * @param done - a callback function
- * @returns {*}
- */
-History.deleteDecisionInstancesAsync = function(params, done) {
-  if (typeof params === 'function') {
-    done = arguments[0];
-    params = {};
-  }
-
-  return this.http.post(this.path + '/decision-instance/delete', {
-    data: params,
-    done: done
-  });
-};
-
 
 /**
  * Query for historic batches that fulfill given parameters. Parameters may be the properties of batches, such as the id or type.
@@ -4189,14 +4151,6 @@ var ProcessDefinition = AbstractClientResource.extend(
       return AbstractClientResource.list.apply(this, arguments);
     },
 
-    /**
-     * Get a count of process definitions
-     * Same parameters as list
-     */
-    count: function() {
-      return AbstractClientResource.count.apply(this, arguments);
-    },
-
 
   /**
    * Fetch the variables of a process definition
@@ -4305,17 +4259,17 @@ var ProcessDefinition = AbstractClientResource.extend(
 
       var queryParams = '?';
       var param = 'cascade';
-      if (typeof data[param] === 'boolean') {
-        queryParams += param + '=' + data[param];
+      if (data[param]) {
+        queryParams += param + '=true';
       }
 
       param = 'skipCustomListeners';
-      if (typeof data[param] === 'boolean') {
+      if (data[param]) {
         if (queryParams.length > 1) {
           queryParams += '&';
         }
 
-        queryParams += param + '=' + data[param];
+        queryParams += param + '=true';
       }
 
       return this.http.del(this.path +'/'+ pointer + queryParams, {
@@ -5625,13 +5579,14 @@ User.create = function(options, done) {
  * @param  {Function} done
  */
 User.list = function(options, done) {
-  if (typeof options === 'function') {
+  if (arguments.length === 1) {
     done = options;
     options = {};
   }
   else {
     options = options || {};
   }
+
   return this.http.get(this.path, {
     data: options,
     done: done || noop
@@ -5759,21 +5714,6 @@ User.delete = function(options, done) {
   var id = typeof options === 'string' ? options : options.id;
 
   return this.http.del(this.path + '/' + utils.escapeUrl(id), {
-    done: done || noop
-  });
-};
-
-
-/**
- * Unlock a user
- * @param  {Object|uuid} options You can either pass an object (with at least a id property) or the id of the user to be unlocked
- * @param  {uuid} options.id
- * @param  {Function} done
- */
-User.unlock = function(options, done) {
-  var id = typeof options === 'string' ? options : options.id;
-
-  return this.http.get(this.path + '/' + utils.escapeUrl(id) + '/unlock', {
     done: done || noop
   });
 };
@@ -5931,23 +5871,6 @@ Variable.instances = function(params, done) {
     done: done
   });
 };
-
-/**
- * Get a count of variables
- * Same parameters as instances
- */
-
-Variable.count = function(params, done) {
-  var path = this.path + '/count';
-
-  return this.http.post(path, {
-    data: params,
-    done: done
-  });
-};
-
-
-
 
 module.exports = Variable;
 
